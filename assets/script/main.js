d72b6a70a558f990fbb266254998a82504db2cef
Philippe Cossette <philcossette@hotmail.com>
2025-01-12 16:21:57 -0500

    Added Second Date For countdown reach and fixed the scroll

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,14 +1,29 @@
+let countDownInterval;
+
 function main(){
-    countDown();
+    const dateGab = new Date("2021-01-23T23:59:59");
+    const datePhil = new Date("2025-03-10T23:59:59");
+    const timeNow = new Date().getTime();
+    if(dateGab.getTime() > timeNow){
+        countDown(dateGab);
+    }
+    else {
+        countDown(datePhil);
+    }
 }
 
-function countDown() {
-    const targetDate = new Date("2025-01-23T23:59:59").getTime();
+function countDown(targetDate) {
 
-    const countDown = setInterval(() => {
+    if (countDownInterval){
+        clearInterval(countDownInterval);
+    }
+
+    const targetTime =targetDate.getTime();
+
+    countDownInterval = setInterval(() => {
         const timeNow = new Date().getTime();
-        const timeLeft = targetDate - timeNow;
-
+        const timeLeft = targetTime - timeNow;
+        
         if (timeLeft > 0) {
             const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
             const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -21,10 +36,17 @@ function countDown() {
             document.querySelector(".seconds").textContent = seconds;
         }
         else {
+            clearInterval(countDownInterval);
 
+            // Update DOM to show zero
+            document.querySelector(".days").textContent = "0";
+            document.querySelector(".hours").textContent = "0";
+            document.querySelector(".minutes").textContent = "0";
+            document.querySelector(".seconds").textContent = "0";
+
+            console.log("Countdown has ended!");
         }
     });
 }
 
 main();
-console.log("hello");
